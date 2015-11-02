f8bdb536cbdfe5bdce5c51cfd4b62496d3b0fe08
Koidl Mario <mario.koidl@admiral.at>
2015-11-02 20:01:14 +0100

    Update site map logic

--- SiteMapHandler/Handlers/SiteMapHandler.cs
+++ SiteMapHandler/Handlers/SiteMapHandler.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Web;
+using System.Web.UI;
 using System.Xml;
+using SiteMapHandler.Helpers;
 
 namespace SiteMapHandler.Handlers
 {
@@ -12,28 +17,125 @@ namespace SiteMapHandler.Handlers
         {
             context.Response.ClearHeaders();
             context.Response.ClearContent();
-            context.Response.ContentType = "text/xml";
 
-            var writer = new XmlTextWriter(context.Response.Output);
-            writer.WriteStartDocument();
-            writer.WriteStartElement("urlset", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            if (context.Request.CurrentExecutionFilePathExtension == ".gz")
+            {
+                context.Response.ContentType = "application/x-gzip";
+                int mandatorNumber = 1;
 
-            writer = SetItem(writer);
+                using (var gzipStream = new GZipStream(context.Response.OutputStream, CompressionMode.Compress))
+                {
+                    using (var writer = XmlWriter.Create(gzipStream, new XmlWriterSettings()))
+                    {
+                        var sitemap = GetSiteMap(mandatorNumber);
+                        XmlHelper.TransformSiteMapData(writer, sitemap);
+                    }
+                }
+            }
+            else if (context.Request.CurrentExecutionFilePathExtension == ".html")
+            {
+                context.Response.ContentType = "text/html";
+                int mandatorNumber = 1;
 
-            writer.WriteEndDocument();
-            writer.Flush();
+                var sitemap = GetSiteMap(mandatorNumber);
+
+                using (var stringWriter = new StringWriter())
+                {
+                    using (var htmlWriter = new HtmlTextWriter(stringWriter))
+                    {
+                        htmlWriter.WriteLine("<!DOCTYPE html>");
+                        htmlWriter.WriteLine("<head><title>Sitemap</title></head><body style=\"background-color:#00285c\">");
+
+                        htmlWriter.AddAttribute(HtmlTextWriterAttribute.Src, "https://admiral.at/img4/Admiral_white.png");
+                        htmlWriter.AddAttribute(HtmlTextWriterAttribute.Width, "250");
+                        htmlWriter.AddAttribute(HtmlTextWriterAttribute.Height, "40");
+                        htmlWriter.AddAttribute(HtmlTextWriterAttribute.Alt, "www.admiral.at");
+                        htmlWriter.RenderBeginTag(HtmlTextWriterTag.Img);
+                        htmlWriter.RenderEndTag();
+                        htmlWriter.WriteLine(Environment.NewLine);
+
+                        foreach (var link in sitemap)
+                        {
+                            htmlWriter.RenderBeginTag(HtmlTextWriterTag.Div);
+                            htmlWriter.AddAttribute(HtmlTextWriterAttribute.Href, link.Url);
+                            htmlWriter.AddAttribute(HtmlTextWriterAttribute.Style, "color:#ffcc00");
+                            htmlWriter.RenderBeginTag(HtmlTextWriterTag.A);
+                            htmlWriter.Write(link.Url);
+                            htmlWriter.RenderEndTag();
+                            htmlWriter.RenderEndTag();
+                            htmlWriter.WriteLine(Environment.NewLine);
+                        }
+
+                        htmlWriter.WriteLine("</body></html>");
+                        context.Response.Write(stringWriter.ToString());
+                    }
+                }
+            }
+            else
+            {
+                context.Response.ContentType = "application/xml";
+                int mandatorNumber = 1;
+
+                using (var writer = XmlWriter.Create(context.Response.Output, new XmlWriterSettings()))
+                {
+                    var sitemap = GetSiteMap(mandatorNumber);
+                    XmlHelper.TransformSiteMapData(writer, sitemap);
+                }
+            }
         }
 
-        private XmlTextWriter SetItem(XmlTextWriter writer)
+        internal IList<SiteMapData> GetSiteMap(int mandatorNumber)
         {
-            writer.WriteStartElement("url");
-            writer.WriteElementString("loc", "http://www.example.at");
-            writer.WriteElementString("lastmod", DateTime.Now.ToString("yyyy-MM-dd"));
-            writer.WriteElementString("changefreq", "daily");
-            writer.WriteElementString("priority", "1.0");
-            writer.WriteEndElement();
-
-            return writer;
+            return new List<SiteMapData>
+            {
+                new SiteMapData
+                {
+                    Url = "http://cpp.cloudapp.net/sitemap.html",
+                    LastModified = DateTime.Parse("02.11.2015"),
+                    ChangeFrequenc = ChangeFrequenc.Weekly,
+                    Priority = "0.5"
+                },
+                new SiteMapData
+                {
+                    Url = "http://cpp.cloudapp.net/sitemap.xml",
+                    LastModified = DateTime.Parse("02.11.2015"),
+                    ChangeFrequenc = ChangeFrequenc.Weekly,
+                    Priority = "0.5"
+                },
+                new SiteMapData
+                {
+                    Url = "http://cpp.cloudapp.net/sitemap.xml.gz",
+                    LastModified = DateTime.Parse("02.11.2015"),
+                    ChangeFrequenc = ChangeFrequenc.Weekly,
+                    Priority = "0.5"
+                },
+                new SiteMapData
+                {
+                    Url = "http://cpp.cloudapp.net/robots.txt",
+                    LastModified = DateTime.Parse("02.11.2015"),
+                    ChangeFrequenc = ChangeFrequenc.Weekly,
+                    Priority = "0.5"
+                }
+            };
         }
+    }
+
+    public class SiteMapData
+    {
+        public string Url { get; set; }
+        public DateTime LastModified { get; set; }
+        public ChangeFrequenc ChangeFrequenc { get; set; }
+        public string Priority { get; set; }
+    }
+
+    public enum ChangeFrequenc
+    {
+        Always,
+        Hourly,
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly,
+        Never
     }
 }
---
+++ SiteMapHandler/Helpers/XmlHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml;
+using SiteMapHandler.Handlers;
+using System.Collections.Generic;
+
+namespace SiteMapHandler.Helpers
+{
+    internal static class XmlHelper
+    {
+        internal static void TransformSiteMapData(XmlWriter writer, IList<SiteMapData> data)
+        {
+            writer.WriteStartDocument();
+            writer.WriteStartElement("urlset", "http://www.sitemaps.org/schemas/sitemap/0.9");
+
+            foreach (var link in data)
+            {
+                // http://www.sitemaps.org/protocol.html
+
+                writer.WriteStartElement("url");
+                writer.WriteElementString("loc", link.Url);
+                writer.WriteElementString("lastmod", link.LastModified.ToString("yyyy-MM-dd"));
+                writer.WriteElementString("changefreq", link.ChangeFrequenc.ToString());
+                writer.WriteElementString("priority", link.Priority);
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndDocument();
+            writer.Flush();
+        }
+    }
+}
